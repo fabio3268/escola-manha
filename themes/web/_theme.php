ea82ba60548e586d10fdf25d58abd9b8c9affe75
Fábio Santos <fabio3268@gmail.com>
2023-05-05 08:03:58 -0300

    Estrutura MVC - Start

--- themes/web/_theme.php
+++ themes/web/_theme.php
@@ -2,16 +2,15 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script type="text/javascript" src="<?= url("/assets/web/scripts.js"); ?>" async></script>
+    <link rel="stylesheet" href="<?= url("/assets/web/styles.css"); ?>">
 </head>
 <body>
 
-    <?php
-        echo $this->section("content");
-    ?>
+<?php
+    echo $this->section("content");
+?>
 
 </body>
 </html>
