ef12a348defda3ad7b65f0edda3269d17536ce80
Fábio Santos <fabio3268@gmail.com>
2023-09-06 09:38:16 -0300

    Fazendo requisições a API

--- themes/web/api-login.php
+++ themes/web/api-login.php
@@ -2,14 +2,32 @@
 
 <div>
     <form id="formLogin">
-        <div>E-mail: <input type="text" value="fabiosantos@ifsul.edu.br"></div>
-        <div>Password: <input type="text" value="12345678"></div>
+        <div>E-mail: <input type="text" id="email" name="email" value="fabiosantos@ifsul.edu.br"></div>
+        <div>Password: <input type="text" id="password" name="password" value="12345678"></div>
         <button type="submit">Login</button>
     </form>
 </div>
-
 <script type="module" async>
-
     import {request, requestDebugError} from "<?php echo url("/assets/_shared/functions.js"); ?>";
-
+    const formLogin = document.querySelector("#formLogin");
+    formLogin.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log("oi");
+        const urlLogin = "<?= url("api/user/login"); ?>";
+        console.log(urlLogin);
+        const options = {
+            method : "get",
+            headers : {
+                email : document.querySelector("#email").value,
+                password : document.querySelector("#password").value
+            }
+        };
+        fetch(urlLogin,options).then(response => {
+            response.json().then(user => {
+                //console.log(user);
+                //console.log(JSON.stringify(user));
+                localStorage.setItem("userLogin",JSON.stringify(user));
+            });
+        });
+    });
 </script>
