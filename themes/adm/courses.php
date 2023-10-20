ea1d13d74739364d0ce92be5acc8d12c7b29f99b
Fábio Santos <fabio3268@gmail.com>
2023-10-20 10:07:41 -0300

    Cursos - CRUD

--- themes/adm/courses.php
+++ themes/adm/courses.php
@@ -30,21 +30,86 @@
         </tbody>
     </table>
 </div>
+
+<!-- Modal para editar Cursos -->
+<div id="edit-modal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2>Editar Curso</h2>
+        <form id="edit-form">
+            <input type="hidden" id="id" name="id">
+            <label for="name">Nome:</label>
+            <input type="text" id="name" name="name">
+            <label for="category_id">Categoria:</label>
+            <select id="category_id" name="category_id">
+                <option value="">Selecione uma Categoria</option>
+
+                <?php
+                foreach ($categories as $category) {
+                    echo "<option value='{$category->id}'>{$category->name}</option>";
+                }
+                ?>
+
+            </select>
+            <label for="price">Preço:</label>
+            <input type="text" id="price" name="price">
+            <button type="submit">Salvar</button>
+        </form>
+    </div>
+</div>
+
 <script type="text/javascript">
+
+    const tableCourses = document.querySelector("table");
+    // Seletor para a modal
+    const modal = document.querySelector("#edit-modal");
+    // Seletor para o botão de fechar a modal
+    const closeModalButton = document.querySelector(".close");
+
+    // Função para abrir a modal com dados do produto (vai receber por parâmetro o id do produto)
+    function openModal() {
+        modal.style.display = "block";
+    }
+
+    // Fechar a modal ao clicar no botão de fechar
+    closeModalButton.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    // Fechar a modal quando o usuário clicar fora dela
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    };
+
+    // Função para carregar os dados do produto na modal
+
+    tableCourses.addEventListener("click", (event) => {
+
+        if(event.target.tagName === "TD"){
+            console.log(`Mostrar: ${event.target.parentNode.getAttribute("data-id")}`);
+            openModal();
+        }
+
+        if(event.target.tagName === "BUTTON"){
+            console.log(`Apagar: ${event.target.parentNode.parentNode.getAttribute("data-id")}`);
+            //event.target.parentNode.parentNode.remove();
+        }
+    });
+
     const selectCategory = document.querySelector("#category");
     selectCategory.addEventListener("change",  async () => {
-        console.log(selectCategory.value);
         const url = "<?= url("api/courses/category/"); ?>" + selectCategory.value;
         const response = await fetch(url, {
             method : "get"
         });
         const courses = await response.json();
-        console.log(courses);
         const listCourses = document.querySelector("#courseList");
         listCourses.innerHTML = "";
         courses.forEach((course) => {
-            console.log(course.name);
             const tr = document.createElement("tr");
+            tr.setAttribute("data-id", course.id);
             tr.innerHTML = `<td>${course.id}</td><td>${course.name}</td><td>R$ ${course.price}</td><td><button>X</button></td>`;
             listCourses.appendChild(tr);
         });
